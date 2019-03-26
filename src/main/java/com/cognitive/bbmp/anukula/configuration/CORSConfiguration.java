c6459bc81e4817b635c3a0e40d6ee04eed11155f
Vishnu Hari <vishnuprasadh@gmail.com>
2019-03-26 22:29:11 +0530

    Configuration tag missing and hence added

--- src/main/java/com/cognitive/bbmp/anukula/configuration/CORSConfiguration.java
+++ src/main/java/com/cognitive/bbmp/anukula/configuration/CORSConfiguration.java
@@ -1,9 +1,13 @@
 package com.cognitive.bbmp.anukula.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+@Configuration
+@EnableWebMvc
 public class CORSConfiguration implements WebMvcConfigurer {
 
 	@Override
